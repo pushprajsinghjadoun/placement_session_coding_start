d338f1130a0cd3264016ca4ad0e67161010dac14
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-08-05 13:51:29 +0530

    sizeof datatype

---
+++ basic_question/sizeofdatatypes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 0;    // 4
+    long int l = 12345569; // 4
+    long long int ll = 123456546554569;  // 8
+    char c= '*';   // 1 
+    string s = "pushpraj";   //  24
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int sizeofarr = sizeof(arr)/sizeof(arr[0]);
+    cout<<sizeof(i)<<"  "<<sizeofarr;
+    return 0;
+}
