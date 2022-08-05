25dc7d6ae6175fba2ecf1da48abdfaaa6760a608
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-08-05 13:36:00 +0530

    hello world & deciaml to binary

--- basic_question/decimal_to_binary.cpp
+++ basic_question/decimal_to_binary.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello world";
+    int n;
+    vector<int> v;
+
+    cin>>n;
+    while(n)
+    {
+     int r = n%2;
+     n = n/2;
+     v.push_back(r);   
+    }
+    
+    for(int i=v.size()-1;i>-1;i--)
+    {
+        cout<<v[i]<<" ";
+    }
+
+
     return 0;
 }
