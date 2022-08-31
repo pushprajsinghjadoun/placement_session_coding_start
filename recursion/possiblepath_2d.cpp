81ac2a608d3ba1a7cae19206c3e98365a0c5e677
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-08-31 15:32:11 +0530

    dice path

--- recursion/possiblepath_2d.cpp
+++ recursion/possiblepath_2d.cpp
@@ -13,11 +13,24 @@ void printallpath(int i,int j,int n,int m,string s)
     printallpath(i,j+1,n,m,s=s+"D");
     printallpath(i+1,j,n,m,s=s+"R");
 }
+void dicepath(int n, int i,string s)
+{
+    if(i>=n) return;
+    if(i==n-1)
+    {
+        cout<<s<<endl;
+        return;
+    }
+    for(int j=1;j<=n-1;j++)
+    {
+        dicepath(n,i+j,s+to_string(j)+"->");
+    }
+}
 
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    printallpath(0,0,3,3,"");
+    
+    //printallpath(0,0,3,3,"");
+    dicepath(7,0,"");
     return 0;
 }
