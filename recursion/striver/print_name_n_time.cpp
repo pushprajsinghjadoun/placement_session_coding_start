eaa738147362a48d693dbed3aad21dc3410e7b18
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-09-14 15:13:39 +0530

    1 to n printing

--- recursion/striver/print_name_n_time.cpp
+++ recursion/striver/print_name_n_time.cpp
@@ -8,13 +8,20 @@ void name(string na,int n)
 
     name(na,n-1);
 }
+void one_to_n(int n)
+{
+    if(n==0) return;
+    one_to_n(n-1);
+    cout<<n<<" ";
+}
 
 int main()
 {
     int n;
     cin>>n;
-    string na;
-    cin>>na;
-    name(na,n);
+    // string na;
+    // cin>>na;
+    // name(na,n);
+    one_to_n(n);
     return 0;
 }
