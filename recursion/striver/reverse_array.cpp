673ece047602da18a1bb35cd10be9f3bff65e658
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-09-14 15:45:47 +0530

    Check palidrome using recursion

--- recursion/striver/reverse_array.cpp
+++ recursion/striver/reverse_array.cpp
@@ -8,19 +8,30 @@ void swap_array(int *arr,int l, int r)
     swap_array(arr,l+1,r);
 }
 
+bool ispali(string s,int l,int r)
+{
+    if(l>=r/2) return true;
+    if(s[l]!=s[r-l]) return false;
+    return ispali(s,l+1,r);
+}
+
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    swap_array(arr,0,n-1);
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0;i<n;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // swap_array(arr,0,n-1);
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    string s;
+    cin>>s;
+    int size = s.size();
+    cout<<ispali(s,0,size-1);
     return 0;
 }
