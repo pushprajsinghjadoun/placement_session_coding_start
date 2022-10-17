9978e4a2b9bc6a66a845e55b233776c04c3048d2
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-10-17 12:19:20 +0530

    inheritance

--- OOP/constructor.cpp
+++ OOP/constructor.cpp
@@ -6,13 +6,23 @@ class Pushpraj
     public:
     Pushpraj()
     {
-        cout<<"Hello i am constructor";
+        cout<<"Hello i am Pushpraj"<<endl;
+    }
+};
+
+class Lakshyaraj:Pushpraj
+{
+    public:
+    Lakshyaraj()
+    {
+        cout<<"i am Lakshyaraj"<<endl;
     }
 };
 
 int main()
 {
     Pushpraj o;
+    Lakshyaraj l;
 
     return 0;
 }
