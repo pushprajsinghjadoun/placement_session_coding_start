54b155672829db47468f89f98933146035ec554a
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-10-17 12:09:36 +0530

    Verify constructor work

---
+++ OOP/constructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Pushpraj
+{
+    public:
+    Pushpraj()
+    {
+        cout<<"Hello i am constructor";
+    }
+};
+
+int main()
+{
+    Pushpraj o;
+
+    return 0;
+}
