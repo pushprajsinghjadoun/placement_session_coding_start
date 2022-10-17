ac9e9a7c71d7e27370b5354b01ad8ff595334a72
Pushpraj Singh Jadoun <pushprajsinghjadoun@outlook.com>
2022-10-17 12:55:17 +0530

    Operator overloading

---
+++ OOP/operator_overloading.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class add
+{
+    int a,b;
+    public:
+    add(int a1, int b1)
+    {
+        this->a = a1;
+        this->b = b1;
+    }
+    
+    add operator+(add c)
+    {
+        return add(a+c.a,b+c.a);
+    }
+    void display()
+    {
+        cout<<this->a<<" "<<this->b<<endl;
+    }
+};
+
+int main()
+{
+    add o(2,3);
+    add o2(5,6);
+    add o3 = o+o2;
+    o3.display();
+    return 0;
+}
